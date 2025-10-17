b93e64eb3c24818b220958f479f724ad7c6f2908
Rodrigo Mayorga Castelan <151226868+rmayorgac@users.noreply.github.com>
2025-10-16 23:26:35 -0600

    Add files via upload

---
+++ Practica5/js/foto.js
@@ -0,0 +1,31 @@
+function createImage(src){
+    const image = document.createElement('img');
+    image.src = src;
+    return image;
+}
+
+const albumView = document.querySelector('#album-view');
+for (let i = 0; i < PHOTO_LIST.length; i++){
+    const photoSrc = PHOTO_LIST[i];
+    const image = createImage(photoSrc);
+    image.addEventListener('click', onThumbnailClick);
+    albumView.appendChild(image);
+}
+
+function onThumbnailClick(event){
+    const image = createImage(event.currentTarget.src);
+    document.body.classList.add('no-scroll');
+    const modalView = document.querySelector('#modal-view');
+    if (!modalView) return;
+    modalView.appendChild(image);
+    modalView.classList.remove('hidden');
+    
+}
+function onModalClick(){
+    document.body.classList.remove('no-scroll');
+    modalView.classList.add('hidden');
+    modalView.innerHTML = '';
+
+}
+const modalView = document.querySelector('#modal-view');
+modalView.addEventListener('click', onModalClick);
